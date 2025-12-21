5f133c5e0a0e305945773b424498da81fe45d411
Pranav Khaire <pranav.22110507@viit.ac.in>
2025-12-21 22:42:01 +0530

    remove import @vercel/analytics/next

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 import AllPgList from "./pages/AllPgList";
-import { Analytics } from '@vercel/analytics/next';
 
 const queryClient = new QueryClient();
 
@@ -41,7 +40,6 @@ const App = () => (
                             </Routes>
                         </main>
                         <Footer />
-                        <Analytics />
                     </div>
                 </AuthProvider>
             </BrowserRouter>
